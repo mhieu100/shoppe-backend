3d1fe04ff05f6bc9780adde5d6a66821bc9a3d30
mhieu100 <hieunguyen201103@gmail.com>
2025-02-17 20:57:42 +0700

    update exception and response

---
+++ src/main/java/com/project/exception/GlobalException.java
@@ -0,0 +1,21 @@
+package com.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.project.model.Response;
+
+@RestControllerAdvice
+public class GlobalException {
+    @ExceptionHandler(value = {
+            NotFoundException.class,
+    })
+    public ResponseEntity<Response<Object>> handleException(Exception ex) {
+        Response<Object> res = new Response<>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+}
---
+++ src/main/java/com/project/model/Response.java
@@ -0,0 +1,19 @@
+package com.project.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response<T>{
+    int statusCode;
+    String error;
+    Object message;
+    T data;
+}
